e6999be92f0e4a5568c0d5f763ac31fe72189bc5
KarynaMisnik <karinamisnik94@gmail.com>
2026-06-17 14:30:00 +0300

    park details modal added

--- src/app/ParkDetails.tsx
+++ src/app/ParkDetails.tsx
@@ -1,23 +1,50 @@
 import { useRouter } from "expo-router";
-import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { useState } from "react";
+import { Modal, Pressable, Text, View } from "react-native";
 
 export default function ParkDetails() {
   const router = useRouter();
+  const [visible, setVisible] = useState(false);
   return (
     <View>
       <Text>Park Name</Text>
       <Text>Description</Text>
       <Text>Img here</Text>
       <Pressable
+        onPress={() => setVisible(true)}
         style={{
           backgroundColor: "green",
           padding: 12,
           borderRadius: 8,
         }}
       >
-        View Trails
+        View Trails Modal
       </Pressable>
+
+      <Modal visible={visible} transparent={true} animationType="fade">
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0,0,0,0.5)",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 20,
+              borderRadius: 12,
+            }}
+          >
+            <Text>Hello from modal!</Text>
+
+            <Pressable onPress={() => setVisible(false)}>
+              <Text>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
