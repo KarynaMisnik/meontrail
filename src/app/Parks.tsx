ca5687ba69684b4ad5d3554237836f39d79253f6
KarynaMisnik <karinamisnik94@gmail.com>
2026-06-19 14:30:00 +0300

    trail data onscroll

--- src/app/Parks.tsx
+++ src/app/Parks.tsx
@@ -1,11 +1,23 @@
 import { useRouter } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+import { parks } from "../data/parks";
 
 export default function Parks() {
   const router = useRouter();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Parks Page</Text>
+      <FlatList
+        data={parks}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+          </View>
+        )}
+      />
+
       <Pressable
         onPress={() => router.push("/ParkDetails")}
         style={{
@@ -30,5 +42,21 @@ const styles = StyleSheet.create({
   text: {
     color: "white",
     fontSize: 20,
+  },
+  card: {
+    marginBottom: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "white",
+  },
+  description: {
+    fontSize: 14,
+    color: "white",
   },
 });
